d03c393710e32bc3fb8d6a597b94c917802a46f8
TruyenGau <108222539+TruyenGau@users.noreply.github.com>
2023-03-08 18:29:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Lợi Ngày Quốc Tế 08/03 vui vẻ';
+const titleCard = '';
+const contentCard = "Chúc Lợi ngày 08/03 vui vẻ nhận được nhiều quà, nhiều hoa, nhiều lời khen. Chúc Lợi gặp nhiều may mắn hơn, hạnh phúc hơn nhiều niềm vui, nhiều điều tuyệt diệu hơn, đặc biệt thành công trong học tập :3";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'b.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
